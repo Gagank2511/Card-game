994902f1c43519cf5cbe62899303b8104606db4a
Kaur <gagandeepmalh@gmail.com>
2026-01-11 15:59:36 +0000

    Explain move semantics and polymorphism in Dealer implementation

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,8 +1,32 @@
 #include "Dealer.h"
 
+/*
+ * DEALER CLASS IMPLEMENTATION
+ * ---------------------------
+ * Demonstrates Strategy Pattern and Smart Pointer usage.
+ */
+
 Dealer::Dealer(std::unique_ptr<DrawStrategy> s)
-    : strategy(std::move(s)) {}
+    : strategy(std::move(s)) {
+    /*
+     * MOVE SEMANTICS:
+     * - std::move transfers ownership of the unique_ptr
+     * - After this, the original pointer is null
+     * - Dealer now owns the strategy exclusively
+     *
+     * This is more efficient than copying and ensures
+     * there's only one owner of the strategy object.
+     */
+}
 
 bool Dealer::shouldDraw() {
+    /*
+     * STRATEGY PATTERN IN ACTION:
+     * - Dealer doesn't know which strategy is being used
+     * - Calls the interface method shouldDraw()
+     * - Polymorphism ensures the correct implementation runs
+     *
+     * This allows changing dealer behavior without modifying this code.
+     */
     return strategy->shouldDraw(getScore());
 }
