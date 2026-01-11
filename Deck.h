a080d4306b67bf834f39aaffe0093c6e98b6569d
Kaur <gagandeepmalh@gmail.com>
2026-01-11 15:57:15 +0000

    Improve Deck header with memory management documentation and isEmpty method

--- Deck.h
+++ Deck.h
@@ -4,16 +4,28 @@
 #include "Card.h"
 #include "CardFactory.h"
 
+/*
+ * DECK CLASS
+ * ----------
+ * Manages a collection of Card pointers using dynamic memory allocation.
+ *
+ * POINTER USAGE JUSTIFICATION:
+ * - Uses Card** to store polymorphic Card objects
+ * - Enables storing different card types (NormalCard, FaceCard, AceCard) together
+ * - Dynamic allocation allows deck size to be set at runtime
+ */
 class Deck {
 private:
-    Card** cards;
-    int size;
+    Card** cards;       // Pointer to array of Card pointers (dynamic array)
+    int capacity;       // Total number of cards created
+    int currentIndex;   // Index of next card to draw
 
 public:
     Deck(int s);
-    Card* drawCard();
-    int getSize() const;
-    ~Deck();
+    Card* drawCard();       // Returns ownership of card to caller
+    int getSize() const;    // Cards remaining
+    bool isEmpty() const;   // Check if deck is empty
+    ~Deck();                // Cleans up remaining cards
 };
 
 #endif
