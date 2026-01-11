45d0a0198ffb4e08734d7ba63bef8f87d19bc335
Kaur <gagandeepmalh@gmail.com>
2026-01-11 15:56:45 +0000

    Add card suits and AceCard creation to CardFactory

--- CardFactory.cpp
+++ CardFactory.cpp
@@ -1,12 +1,45 @@
 #include "CardFactory.h"
 #include <cstdlib>
 
-Card* CardFactory::createCard(int number) {
-    if (number >= 2 && number <= 10)
-        return new NormalCard(number, to_string(number));
+/*
+ * CARDFACTORY IMPLEMENTATION
+ * --------------------------
+ * Factory Design Pattern - creates appropriate Card subclass based on input.
+ *
+ * MEMORY ALLOCATION:
+ * - Uses 'new' to allocate Card objects on the heap
+ * - Returns pointer to caller who becomes the owner
+ * - Caller is responsible for calling 'delete' when done
+ */
 
-    int r = rand() % 3;
-    if (r == 0) return new FaceCard("Jack");
-    if (r == 1) return new FaceCard("Queen");
-    return new FaceCard("King");
+Card* CardFactory::createCard(int number, const string& suit) {
+    /*
+     * Card creation logic:
+     * - 1 = Ace (special card worth 1 or 11)
+     * - 2-10 = Normal cards (worth face value)
+     * - 11 = Jack, 12 = Queen, 13 = King (all worth 10)
+     *
+     * The 'new' keyword allocates memory on the heap.
+     * We return a base class pointer (Card*) but the actual
+     * object is a derived type - this is POLYMORPHISM.
+     */
+
+    if (number == 1) {
+        // Ace - special card with dual value
+        return new AceCard();
+    }
+    else if (number >= 2 && number <= 10) {
+        // Normal numbered cards
+        string cardName = to_string(number) + " of " + suit;
+        return new NormalCard(number, cardName);
+    }
+    else if (number == 11) {
+        return new FaceCard("Jack of " + suit);
+    }
+    else if (number == 12) {
+        return new FaceCard("Queen of " + suit);
+    }
+    else {
+        return new FaceCard("King of " + suit);
+    }
 }
