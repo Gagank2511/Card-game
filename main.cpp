e4ed808eeb2144eed84c31152ae0987aa894f5b6
Kaur <gagandeepmalh@gmail.com>
2026-01-11 16:38:15 +0000

    Add commenyys about changes

--- main.cpp
+++ main.cpp
@@ -19,28 +19,54 @@
  * - Encapsulation (private data, public interface)
  *
  * MODERN C++:
- * - Smart pointers (unique_ptr for Strategy)
+ * - Smart pointers (unique_ptr for automatic memory management)
  * - Move semantics
+ * - Lambda functions (functional programming)
+ * - Exception handling (try-catch blocks)
+ *
+ * SCALABILITY:
+ * - GameConfig structure for easy customisation
+ * - Difficulty presets (Easy, Normal, Hard)
  *
  * Author: Student
  * Module: Advanced Programming
  */
 
 #include "Game.h"
+#include "GameConfig.h"  // For game configuration
 #include <ctime>
 #include <cstdlib>
+#include <iostream>
 
 int main() {
     // Seed random number generator for card shuffling
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    // Create game object on the stack
-    // Game internally manages heap-allocated objects
-    Game game;
+    /*
+     * USING GAMECONFIG FOR SCALABILITY:
+     *
+     * You can create a game with different settings:
+     *
+     * Option 1: Default game
+     *   Game game;
+     *
+     * Option 2: Custom config
+     *   GameConfig config;
+     *   config.startingChips = 500;
+     *   Game game(config);
+     *
+     * Option 3: Use a preset
+     *   Game game(createEasyConfig());
+     *   Game game(createHardConfig());
+     */
+
+    // Using normal (default) configuration
+    Game game(createNormalConfig());
 
     // Start the game
     game.play();
 
     // Game destructor automatically cleans up all resources
+    // (Thanks to smart pointers - no memory leaks!)
     return 0;
 }
